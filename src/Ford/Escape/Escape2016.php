99dec7a7e1a4f57a2d1427768fab83ec5af19ccf
yuanyueshui <yuanyueshui@wepiao.com>
2017-02-13 16:46:52 +0800

    test

---
+++ src/Ford/Escape/Escape2016.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Felix
+ * Date: 2017/2/13
+ * Time: 下午4:45
+ */
+namespace Felix\Ford\Escape;
+
+class Escape2016
+{
+	public function info()
+	{
+		echo "This is Ford Escape2016!<br />";
+	}
+}
