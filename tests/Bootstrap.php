d361b7978213f706053b7b0abc78ffb983203939
yuanyueshui <yuanyueshui@wepiao.com>
2017-02-16 22:51:56 +0800

    test

---
+++ tests/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Felix
+ * Date: 2017/2/16
+ * Time: 下午10:47
+ */
+
+$loader = include __DIR__ . '/../vendor/autoload.php';
+$loader->add('Felix', __DIR__ . '/../src');
